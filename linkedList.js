b984fab604d3352d86558cfa2e14ed4b6b7dd0f2
quinlayen <quinlayen@gmail.com>
2018-01-18 23:29:20 -1000

    getHead() passed

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,39 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+let listObj = {};
+let head = null;
+let tail = null;
 
+function getHead(){
+return listObj.head = null;
+}
+
+function getTail(){
+
+}
+
+function add(){
+
+}
+
+function remove(){
+
+}
+
+function get(){
+
+}
+
+function insert(){
+
+}
+return{
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert:insert
+    }
 }
